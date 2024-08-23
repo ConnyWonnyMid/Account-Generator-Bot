35c21303ac2cd0263d4f9df229db03afb2382f09
SkiHub <111881796+ConnyWonnyMid@users.noreply.github.com>
2024-08-22 17:09:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run("")
+bot.run("MTI3NjAwOTg1OTIzNzY3NTA2OA.Gr1vEZ.3aSa1k8lCYl4DmZ4k1anc286Z9TOW1EaPPNZrU")
